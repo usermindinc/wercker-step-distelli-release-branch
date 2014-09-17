1075e6700c91a6dc6ddcb6920c82ccab0dbc6540
Eric Maland <emaland@usermind.com>
2014-09-16 18:18:51 -0700

    fix git base location

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cd $WERCKER_SOURCE_DIR
+export GIT_DIR=$WERCKER_SOURCE_DIR
 if [ "$WERCKER_GIT_BRANCH" == "master" ]; then $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli push -m "$DISTELLI_APP build from Wercker at $WERCKER_GIT_COMMIT"; fi
 
 echo $*
