ca75ecd80328ea14b50a8a3778a3c43298e7f0ad
Eric Maland <emaland@usermind.com>
2014-09-16 18:15:43 -0700

    fix branch info in distelli

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+cd $WERCKER_SOURCE_DIR
 if [ "$WERCKER_GIT_BRANCH" == "master" ]; then $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli push -m "$DISTELLI_APP build from Wercker at $WERCKER_GIT_COMMIT"; fi
 
 echo $*
